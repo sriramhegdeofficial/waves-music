ad4c9fb7a5a83529b3020d59d2e9d0ffd1985099
sriramhegdeofficial <sriramhegdeofficial@gmail.com>
2021-05-15 20:54:31 +0530

    v10.3

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,28 +1,15 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMusic } from "@fortawesome/free-solid-svg-icons";
-import { isBrowser } from "react-device-detect";
 
 const Nav = ({ libraryStatus, setLibraryStatus }) => {
-  const renderButton = () => {
-    if (isBrowser) {
-      return (
-        <button onClick={() => setLibraryStatus(!libraryStatus)}>
-          Library
-          <FontAwesomeIcon icon={faMusic} />
-        </button>
-      );
-    } else {
-      <button onClick={() => setLibraryStatus(!libraryStatus)}>
-        Library
-        <FontAwesomeIcon icon={faMusic} />
-      </button>;
-    }
-  };
   return (
     <nav>
       <h1>Waves</h1>
-      {renderButton()}
+      <button onClick={() => setLibraryStatus(!libraryStatus)}>
+        Library
+        <FontAwesomeIcon icon={faMusic} />
+      </button>
     </nav>
   );
 };
